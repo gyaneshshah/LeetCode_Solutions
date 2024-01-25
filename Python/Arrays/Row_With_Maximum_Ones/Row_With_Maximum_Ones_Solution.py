dbd0d5dfc6ead52eef75d1326fbcf02880f24c02
Gyanesh Shah <58587405+gyaneshshah@users.noreply.github.com>
2024-01-24 17:36:22 -0800

    Update Row_With_Maximum_Ones_Solution.py

--- Python/Arrays/Row_With_Maximum_Ones/Row_With_Maximum_Ones_Solution.py
+++ Python/Arrays/Row_With_Maximum_Ones/Row_With_Maximum_Ones_Solution.py
@@ -3,9 +3,7 @@ class Solution:
         count = []
         cnt=0
         for arr in mat:
-            cnt=0
-            for no in arr:
-                cnt+=no
+            cnt=sum(arr)
             count.append(cnt)
         maxcnt = max(count)
         return [count.index(maxcnt), maxcnt]
