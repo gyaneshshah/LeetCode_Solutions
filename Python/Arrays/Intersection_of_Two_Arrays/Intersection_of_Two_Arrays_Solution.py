b0d4379b3eb8ec52b3a1b402f626f22e2ef5e5bb
Gyanesh Shah <58587405+gyaneshshah@users.noreply.github.com>
2024-01-30 10:24:30 -0800

    Update Intersection_of_Two_Arrays_Solution.py

---
+++ Python/Arrays/Intersection_of_Two_Arrays/Intersection_of_Two_Arrays_Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1 = list(set(nums1))
+        nums2 = list(set(nums2))
+        ans = [no for no in nums1 if no in nums2]
+        return ans
