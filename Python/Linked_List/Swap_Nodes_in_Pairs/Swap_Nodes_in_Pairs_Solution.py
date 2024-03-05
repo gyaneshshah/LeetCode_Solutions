dd7086895ccdb6af64893040d409f2e3b14d0dab
Gyanesh Shah <58587405+gyaneshshah@users.noreply.github.com>
2024-03-05 09:27:33 -0800

    Update Swap_Nodes_in_Pairs_Solution.py

---
+++ Python/Linked_List/Swap_Nodes_in_Pairs/Swap_Nodes_in_Pairs_Solution.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        new_node = ListNode(0)
+        new_node.next = head
+        prev_node = new_node
+        while head and head.next:
+            first_node = head
+            second_node = head.next
+
+            prev_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+
+            prev_node = first_node
+            head = first_node.next
+
+        return new_node.next
