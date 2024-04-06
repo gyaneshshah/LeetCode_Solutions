4fb747dc24d8eacba7c9510a0d51130112fc01d9
Gyanesh Shah <58587405+gyaneshshah@users.noreply.github.com>
2024-04-05 19:29:59 -0700

    Update Palindrome_Linked_List_Solution.py

---
+++ Python/Linked_List/Palindrome_Linked_List/Palindrome_Linked_List_Solution.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if head is None or head.next is None:
+            return True
+        string = ""
+        while head:
+            string += str(head.val)
+            head = head.next
+        return string == string[::-1]
