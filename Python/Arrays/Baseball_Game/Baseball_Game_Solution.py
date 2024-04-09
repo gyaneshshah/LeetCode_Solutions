1674f66727ca1ab4baac071dd8c1baf59666734a
Gyanesh Shah <58587405+gyaneshshah@users.noreply.github.com>
2024-04-09 09:38:40 -0700

    Update Baseball_Game_Solution.py

--- Python/Arrays/Baseball_Game/Baseball_Game_Solution.py
+++ Python/Arrays/Baseball_Game/Baseball_Game_Solution.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+        sum_record = 0
+        for i, operation in enumerate(operations):
+            if operation == '+':
+                prev = record[-1]
+                bef_prev = record[-2]
+                record.append(prev+bef_prev)
+                sum_record += prev+bef_prev
+            elif operation == 'D':
+                prev = record[-1]
+                record.append(prev*2)
+                sum_record += prev*2
+            elif operation == 'C':
+                sum_record -= record[-1]
+                record.pop()
+            else:
+                record.append(int(operation))
+                sum_record += int(operation)
+        return sum_record
