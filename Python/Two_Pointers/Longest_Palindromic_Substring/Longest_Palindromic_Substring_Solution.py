014257607872a34706731af7c2613b717cd89734
Gyanesh Shah <58587405+gyaneshshah@users.noreply.github.com>
2024-04-25 14:33:21 -0700

    Update Longest_Palindromic_Substring_Solution.py

---
+++ Python/Two_Pointers/Longest_Palindromic_Substring/Longest_Palindromic_Substring_Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        palindrome_length = 0
+        longest_palindrome = ""
+        for i in range(len(s)):
+            for j in range(len(s)-1, i-1, -1):
+                if s[i:j+1] == s[i:j+1][::-1]:
+                    if j-i+1>palindrome_length:
+                        palindrome_length = j-i+1
+                        longest_palindrome = s[i:j+1]
+                    break
+        return longest_palindrome
