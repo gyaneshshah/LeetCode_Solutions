c8c29e762ce462c14886a95ecb97151a1e68da46
Gyanesh Shah <58587405+gyaneshshah@users.noreply.github.com>
2024-04-30 17:29:33 -0700

    Update Reverse_Prefix_of_Word_Solution.py

---
+++ Python/Two_Pointers/Reverse_Prefix_of_Word/Reverse_Prefix_of_Word_Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        for i, char in enumerate(word):
+            if char==ch:
+                return word[i::-1] + word[i+1:]
+        return word
