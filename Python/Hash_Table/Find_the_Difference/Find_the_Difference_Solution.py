d3b7fd9d7816731d0dfadc1dcb0161b269a05176
Gyanesh Shah <58587405+gyaneshshah@users.noreply.github.com>
2024-05-09 16:15:00 -0700

    Update Find_the_Difference_Solution.py

---
+++ Python/Hash_Table/Find_the_Difference/Find_the_Difference_Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        t = sorted(t)
+        s = sorted(s)
+        for i in range(len(s)):
+            if t[i] != s[i]:
+                return t[i]
+        return t[-1]
